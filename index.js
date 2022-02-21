ff7dc7cb71c669dc3bc1b227a52382393fd9d04b
Amritha Govindarajan <g.amritha@gmail.com>
2022-02-21 16:53:44 +0200

    3.14: Phonebook database, step 2

--- index.js
+++ index.js
@@ -38,12 +38,6 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 })
 
-const getRandomId = () => {
-    const min = 1;
-    const max = 50000;
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
 app.post('/api/persons', (request, response) => {
     const body = request.body;
     if(!body.name || !body.number) {
@@ -54,33 +48,17 @@ app.post('/api/persons', (request, response) => {
                 })
     }
 
-    const foundName = persons.find(person => person.name === body.name);
-    if(foundName) {
-        return response
-                .status(400)
-                .json({
-                    error: 'name must be unique',
-                });
-    }
-
-    let id = getRandomId();
-    let foundId = persons.find(person => person.id === id)
-    
-    while(foundId) {
-        id = getRandomId();
-        foundId = persons.find(person => person.id === id)
-    }
-
-    const person = {
-        id: id,
+    const person = new Person({
         name: body.name,
         number: body.number,
-    }
-    persons = persons.concat(person);
-    response.json(person);
+    });
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson);
+    })
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 });
