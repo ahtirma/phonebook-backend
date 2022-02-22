65fde862ee98fa0986924d9e62cc5a4a745c8f11
Amritha Govindarajan <g.amritha@gmail.com>
2022-02-22 12:23:56 +0200

    3.16: Phonebook database, step 4

--- index.js
+++ index.js
@@ -26,16 +26,20 @@ app.get('/api/persons', (request, response) => {
     }) 
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person);
-    });
+        if(person) {
+            response.json(person);
+        } else {
+            response.status(404).end();
+        } 
+    }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id).then(result => {
         response.status(204).end();
-    })
+    }).catch(error => next(error));
 })
 
 app.post('/api/persons', (request, response) => {
@@ -57,6 +61,17 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson);
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if(error.name === 'CastError') {
+        response.status(400).send({error: 'malformatted ID'});
+    }
+    next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
